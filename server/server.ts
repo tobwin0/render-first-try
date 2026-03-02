f308e3ed38ef2ca715bb333c07e419c1e87c827b
tobwin0 <184448769+tobwin0@users.noreply.github.com>
2026-03-02 14:21:33 +0100

    Added postgis database with some data

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,38 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
 
 const app = new Hono();
 
+const postgres = new pg.Pool({
+  connectionString:
+    process.env.DATABASE_URL || "postgresql://postgres:@localhost",
+});
+
 app.get("/api", async (c) => {
   return c.json({ hello: "world" });
+});
+
+app.get("/api/grunnskoler", async (c) => {
+  const result = await postgres.query(
+    `select  skolenavn, eierforhold, antallelever, st_transform(posisjon, 4326)::json as geometry
+     from
+       grunnskoler_519889439f4c490fab3f18303772a702.grunnskole s
+         inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke f on st_contains(f.omrade, s.posisjon)
+         join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn a on f.objid = a.fylke_fk
+         and a.sprak = 'nor'
+     where a.navn = 'Viken'
+`,
+  );
+  return c.json({
+    type: "FeatureCollection",
+    features: result.rows.map(({ geometry, ...properties }) => ({
+      type: "Feature",
+      properties,
+      geometry,
+    })),
+  });
 });
 
 serve(app);
