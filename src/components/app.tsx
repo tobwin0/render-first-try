97830e81974c6d7d9711739fdcdc54f0dc503e0e
tobwin0 <184448769+tobwin0@users.noreply.github.com>
2026-03-02 14:31:27 +0100

    Added Open Layers

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,3 +1,49 @@
+import { useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+import { useGeographic } from "ol/proj.js";
+
+import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
+import { Stroke, Style, Text } from "ol/style.js";
+
+useGeographic();
+
+const layers = [
+  new TileLayer({ source: new OSM() }),
+  new VectorLayer({
+    source: new VectorSource({
+      url: "/geojson/kommuner.geojson",
+      format: new GeoJSON(),
+    }),
+    style: (f) =>
+      new Style({
+        stroke: new Stroke({
+          width: 3,
+          color: "black",
+        }),
+        text: new Text({
+          text: f.getProperties()["name"],
+        }),
+      }),
+  }),
+  new VectorLayer({
+    source: new VectorSource({
+      url: "/api/grunnskoler",
+      format: new GeoJSON(),
+    }),
+  }),
+];
+const map = new Map({
+  view: new View({ center: [10.7, 59.9], zoom: 8 }),
+  layers,
+});
+
 export function App() {
-  return <>Hello world, first time deploying with render</>;
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  return <div ref={mapRef}></div>;
 }
